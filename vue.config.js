5753cd214a15806c70e972223aa98ce6e13b96c2
Adamman48 <adam.piskynagy@gmail.com>
2022-12-01 15:03:30 +0100

    feat: initialized storybook and image-webpack-loader

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,15 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  pluginOptions: {
-    storybook: {
-      allowedPlugins: ["define"],
-    },
+  chainWebpack: (config) => {
+    config.module
+      .rule("images")
+      .test(/\.(gif|png|jpe?g|svg)$/i)
+      .use("image-webpack-loader")
+      .loader("image-webpack-loader")
+      .options({
+        disable: true, // true = won't be used in dev mode
+      })
+      .end();
   },
 });
