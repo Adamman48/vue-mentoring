ef16e494145ff04d48c859685edbbdfd854121ee
Adamman48 <adam.piskynagy@gmail.com>
2022-12-14 21:33:52 +0100

    feat: added styling to movie display

--- src/components/MyMovieDisplay.vue
+++ src/components/MyMovieDisplay.vue
@@ -38,4 +38,66 @@ export default defineComponent({
 });
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.wrapper {
+  font-family: sans-serif;
+
+  img {
+    float: left;
+    height: auto;
+    max-width: 25vw;
+  }
+
+  .info-section {
+    display: inline-block;
+    width: 65vw;
+    margin: 5% 0;
+  }
+
+  header {
+    text-align: left;
+
+    .title {
+      font-size: 2em;
+      max-width: 50%;
+    }
+
+    .rating {
+      margin-left: 5%;
+      border-radius: 50%;
+      border-style: solid;
+      padding: 2%;
+      font-size: 1.5em;
+      color: #a1e66f;
+      border-color: gray;
+    }
+
+    .tag {
+      display: block;
+      margin-top: 2%;
+      max-width: 30%;
+    }
+  }
+
+  .numbers {
+    margin: 2% 0;
+    text-align: left;
+    color: #f65261;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI",
+      Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue",
+      sans-serif;
+    font-size: 1.5em;
+    font-weight: 300;
+
+    .running-time {
+      margin-left: 5%;
+    }
+  }
+
+  .synopsis {
+    display: block;
+    text-align: left;
+    font-size: 1.2em;
+  }
+}
+</style>
