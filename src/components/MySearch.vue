548d8bb008cb6a7a5c03848286758e03cd8b75bb
Adamman48 <adam.piskynagy@gmail.com>
2022-12-14 12:23:52 +0100

    feat: added search component

---
+++ src/components/MySearch.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <input type="text" placeholder="Type here..." />
+    <MyButton :innerText="'search'" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import MyButton from "./MyButton.vue";
+
+export default defineComponent({
+  name: "my-search",
+  components: {
+    MyButton,
+  },
+});
+</script>
+
+<style scoped lang="scss">
+div {
+  width: 100%;
+
+  input {
+    width: 70%;
+    background-color: #2323238b;
+    color: white;
+    padding: 0.6em;
+    font-size: 15pt;
+    outline: none;
+    border: none;
+    border-radius: 0.2em;
+  }
+
+  ::placeholder {
+    color: white;
+    opacity: 1;
+  }
+
+  button {
+    height: 2.5em;
+    margin: 0 0 0 1vw;
+  }
+}
+</style>
--- src/pages/Main.page.vue
+++ src/pages/Main.page.vue
@@ -1,15 +1,18 @@
 <template>
-  <MyButton :inner-text="['title', 'genre']" />
+  <MySearch />
+  <MyButton :innerText="['title', 'genre']" />
 </template>
 
 <script lang="ts">
 import MyButton from "@/components/MyButton.vue";
+import MySearch from "@/components/MySearch.vue";
 import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "MainPage",
   components: {
     MyButton,
+    MySearch,
   },
 });
 </script>
