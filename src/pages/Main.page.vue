aa3307081201ec52939e87e6a36f49fce261d897
Adamman48 <adam.piskynagy@gmail.com>
2022-12-14 13:48:00 +0100

    feat: added elements to main page

--- src/pages/Main.page.vue
+++ src/pages/Main.page.vue
@@ -1,8 +1,20 @@
 <template>
   <MyLogo :size="SizeEnum.S" />
   <MySearch />
-  <MyButton :innerText="['title', 'genre']" />
-  <MyLogo :size="SizeEnum.XS" />
+  <div class="search-toggle">
+    <span>SEARCH BY</span>
+    <MyButton :innerText="['title', 'genre']" />
+  </div>
+  <div class="results-header">
+    <span class="results-total">n movie found</span>
+    <span class="results-toggle">
+      <span>SORT BY</span>
+      <MyButton :innerText="['release date', 'rating']" />
+    </span>
+  </div>
+  <div>
+    <MyLogo :size="SizeEnum.XS" />
+  </div>
 </template>
 
 <script lang="ts">
@@ -27,4 +39,29 @@ export default defineComponent({
 });
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.search-toggle {
+  font-size: 1.25em;
+  font-weight: 500;
+}
+
+.results-header {
+  height: 10vh;
+  width: 100%;
+
+  .results-total {
+    float: left;
+    position: relative;
+    top: 1.2em;
+    font-weight: 700;
+    font-size: 1.25em;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI",
+      Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue",
+      sans-serif;
+  }
+
+  .results-toggle {
+    float: right;
+  }
+}
+</style>
