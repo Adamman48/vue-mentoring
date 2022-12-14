f69ecc654b58f9e1b9f05580f006ee5387db09f1
Adamman48 <adam.piskynagy@gmail.com>
2022-12-14 13:18:33 +0100

    feat: added logo component

---
+++ src/components/MyLogo.vue
@@ -0,0 +1,55 @@
+<template>
+  <div :class="`size-${size}`">
+    <span>netflix</span>
+    <span>roulette</span>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+import { SizeEnum } from "../definitions/MyLogo.definitions";
+
+export default defineComponent({
+  name: "my-logo",
+  props: {
+    size: {
+      type: String as PropType<SizeEnum>,
+      default: SizeEnum.S,
+    },
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.size {
+  &-xs {
+    font-size: 1em;
+  }
+  &-s {
+    font-size: 1.5em;
+  }
+  &-m {
+    font-size: 2em;
+  }
+  &-l {
+    font-size: 2.5em;
+  }
+  &-xl {
+    font-size: 3em;
+  }
+}
+
+div {
+  color: #f65261;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+
+  :nth-child(1) {
+    font-weight: 900;
+  }
+
+  :nth-child(2) {
+    font-weight: 300;
+  }
+}
+</style>
--- src/pages/Main.page.vue
+++ src/pages/Main.page.vue
@@ -1,18 +1,28 @@
 <template>
+  <MyLogo :size="SizeEnum.S" />
   <MySearch />
   <MyButton :innerText="['title', 'genre']" />
+  <MyLogo :size="SizeEnum.XS" />
 </template>
 
 <script lang="ts">
 import MyButton from "@/components/MyButton.vue";
+import MyLogo from "@/components/MyLogo.vue";
 import MySearch from "@/components/MySearch.vue";
+import { SizeEnum } from "@/definitions/MyLogo.definitions";
 import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "MainPage",
+  data() {
+    return {
+      SizeEnum: SizeEnum,
+    };
+  },
   components: {
     MyButton,
     MySearch,
+    MyLogo,
   },
 });
 </script>
