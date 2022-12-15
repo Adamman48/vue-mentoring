225b8be448c1994339d82845d973918ec9a4ada8
Adamman48 <adam.piskynagy@gmail.com>
2022-12-15 10:48:19 +0100

    feat: added footer styling

--- src/pages/Main.page.vue
+++ src/pages/Main.page.vue
@@ -147,4 +147,15 @@ header {
     color: white;
   }
 }
+
+footer {
+  width: 100%;
+  height: 10vh;
+  background-color: #424242;
+
+  .logo-wrapper {
+    position: relative;
+    top: 35%;
+  }
+}
 </style>
